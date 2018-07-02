424c1e5968453f8832c5ce54fcfbf0841f0d230c
Kent Kwong <kentk87@hotmail.com>
2018-07-03 01:13:18 +0800

    add get by id

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5b3a5857efe81a23ec0ac1fd';
+
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+Todo.find({
+  _id: id
+}).then((todos) => {
+  console.log('Todos', todos);
+});
+
+Todo.findOne({
+  _id: id
+}).then((todo) => {
+  console.log('Todo', todo);
+});
+
+Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return console.log('Id not found');
+  }
+  console.log('Todo By Id', todo);
+}).catch((e) => console.log(e));
+
+User.findById('5b39d5e83021a21b9021ae9f').then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
